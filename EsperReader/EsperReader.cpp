dd1cad90f8ee71dd0dbf1cfcfaebefb169e16bd4
Torphedo <ahiatt.cosgrove@gmail.com>
2022-07-19 15:51:33 -0400

    Remove old unused function

--- EsperReader/EsperReader.cpp
+++ EsperReader/EsperReader.cpp
@@ -7,16 +7,6 @@
 using namespace std;
 using std::string;
 
-string getFileExt(const string& s) {
-
-	size_t i = s.rfind('.', s.length());
-	if (i != string::npos) {
-		return(s.substr(i + 1, s.length() - i));
-	}
-
-	return("");
-}
-
 int main(int argc, char* argv[])
 {
 	struct stat meta;
