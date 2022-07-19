065e27797cf5cb5ff960c08d058e3bc4806e0f6a
Torphedo <ahiatt.cosgrove@gmail.com>
2022-07-19 05:00:30 -0400

    Better metadata & printing

--- EsperReader/deck.h
+++ EsperReader/deck.h
@@ -10,8 +10,11 @@ typedef struct Deck
 	char Name[16];
 	short CardData[30];
 	short SchoolCount;
-	short Metadata; // Purpose unknown.
-	char pad[16];
+	short Metadata; // Purpose unknown. Made up of 2 bytes, which seem to always be 0x00 or 0x40.
+	int MissionClears;
+	int MissionAttempts;
+	int MultiplayerWins;
+	int MultiplayerWinRate;
 }deckbin;
 
 void ParseDeckFile(char* filename)
@@ -21,11 +24,20 @@ void ParseDeckFile(char* filename)
 
 	DeckBinary.open(filename, ios::in | ios::binary);
 	DeckBinary.read((char*)&deck, (sizeof(deck)));
-	cout << "Name: " << deck.Name << "\nSchool Count: " << deck.SchoolCount << "\nUnknown Metadata: " << deck.Metadata << "\n";
+	cout << "Name: " << deck.Name << "\nSchool Count: " << deck.SchoolCount << "\nUnknown Metadata: " << deck.Metadata;
+	cout << "\nMission Clears: " << deck.MissionClears << "\nMission Attempts: " << deck.MissionAttempts;
+	cout << "\nMultiplayer Wins: " << deck.MultiplayerWins << "\nMultiplayer Win Rate: " << deck.MultiplayerWinRate << "%\n";
 
 	for (int n = 0; n < 30; n++)
 	{
-		cout << "\nCard #" << (n + 1) << ": ";
+		if (n < 9)
+		{
+			cout << "\nCard #" << (n + 1) << ":   ";
+		}
+		else
+		{
+			cout << "\nCard #" << (n + 1) << ":  ";
+		}
 		printSkillNames(deck.CardData[n]);
 	}
 
