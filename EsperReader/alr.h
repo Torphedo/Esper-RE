a4338a9d4c690ae1e2ac2946c2e22a0b6a8f5933
Torphedo <ahiatt.cosgrove@gmail.com>
2022-07-20 08:53:55 -0400

    Prettier file header printing
    It now prints in hex, with everything perfectly aligned.

--- EsperReader/alr.h
+++ EsperReader/alr.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
@@ -23,21 +24,38 @@ void ParseAlrHeader(fstream& BinaryALR, char* filename)
 	BinaryALR.read((char*)&Header, (sizeof(Header)));
 
 	// Print file data
+	cout << "========== Header ==========\n\n";
 	cout << "Block Magic: " << Header.Magic;
 	cout << "\nHeader Block Size: " << Header.HeaderSize;
 	cout << "\nBlock Flags: " << Header.Flags;
 	cout << "\nWhitespace End Address: " << Header.WhitespaceEndAddr;
 	cout << "\n# of listed block offsets: " << Header.InfoSectionsNum;
-	cout << "\nUnknown Value: " << Header.Unknown << "\n";
+	cout << "\nUnknown Value: " << Header.Unknown << "\n\n";
 
 	// Dynamic int array with size = InfoSectionsNum. Each int is a pointer to a data block.
-	int *array = new int[Header.InfoSectionsNum];
-	BinaryALR.read((char*)array, (sizeof(int) * Header.InfoSectionsNum));
+	int *ptrarray = new int[Header.InfoSectionsNum];
+	BinaryALR.read((char*)ptrarray, (sizeof(int) * Header.InfoSectionsNum));
+	cout << "---------- Pointer Array ----------\n";
 	for (int n = 0; n < 141; n++)
 	{
-		cout << "\nOffset " << (n + 1) << ": " << array[n];
+		cout << "\nPointer " << (n + 1);
+
+		// Different spacings for different #, to make everything print evenly
+		if (n < 9) {
+			cout << ":   0x";
+		}
+		else if (n < 99) {
+			cout << ":  0x";
+		}
+		else {
+			cout << ": 0x";
+		}
+
+		// Convert to hex, fill leading space with 0s, set back to decimal afterwards
+		// so that the pointer # isn't in hex.
+		cout << setfill('0') << setw(sizeof(ptrarray[n]) + 2) << hex << ptrarray[n] << dec;
 	}
-	delete[] array;
+	delete[] ptrarray;
 	return;
 }
 
