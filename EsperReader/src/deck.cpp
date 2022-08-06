40aff47ab0a7fa9215413a58a054b0c25c1d6462
Torphedo <ahiatt.cosgrove@gmail.com>
2022-08-06 02:08:04 -0400

    Nice little ASCII table

--- EsperReader/src/deck.cpp
+++ EsperReader/src/deck.cpp
@@ -17,33 +17,36 @@ void ParseDeckFile(char* filename)
 	cout << "\nMultiplayer Wins: " << deckbin.MultiplayerWins;
 	cout << "\nMultiplayer Win Rate: " << deckbin.MultiplayerWinRate << "%\n";
 
+	cout << "\n\n/////////////////////////////////////////////////////////////////////////////////" << endl;
+
 	for (int n = 0; n < 30; n++)
 	{
-		cout << "\nCard #" << (n + 1); // (n + 1) is used because the counter starts at 0, not 1
+		cout << "|| Card #" << (n + 1); // (n + 1) is used because the counter starts at 0, not 1
 
 		// Slightly different spacing to keep things aligned
-		if (n < 9)
-		{
-			cout << ":   ";
-		}
-		else
-		{
-			cout << ":  ";
-		}
+		if (n < 9) { cout << ":  "; }
+		else { cout << ": "; }
 
-		if (deckbin.CardData[n] == -1)
-		{
+		int spacing = 32;
+
+		if (deckbin.CardData[n] == -1) {
 			cout << "Aura Particle";
+			spacing -= (int)SkillIDs[0].size();
 		}
 		else if (deckbin.CardData[n] >= 394) // IDs 394 - 499 are all copies of Psycho Wave
 		{
 			cout << "Psycho Wave";
+			spacing -= (int)SkillIDs[394].size();
 		}
-		else
-		{
+		else {
 			cout << SkillIDs[deckbin.CardData[n]]; // Print skill name from string array by ID
+			spacing -= (int)SkillIDs[deckbin.CardData[n]].size();
 		}
+		cout << setw(spacing) << setfill(' ');
+		if (n % 2) { cout << "||" << "\n"; }
 	}
+
+	cout << "/////////////////////////////////////////////////////////////////////////////////" << endl;
 
 	DeckBinary.close();
 	return;
