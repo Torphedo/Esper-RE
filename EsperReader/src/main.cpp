bc1e60f138d0b392551757e92bf029993f309b1f
Torphedo <73564623+Torphedo@users.noreply.github.com>
2022-10-23 22:57:49 -0400

    Remove ALR parser
    This wasn't really going anywhere, and I don't really like this program structure. This should be its own program.

--- EsperReader/src/main.cpp
+++ EsperReader/src/main.cpp
@@ -1,6 +1,5 @@
 #include <main.h>
 #include <deck.h>
-#include <alr.h>
 #include <winAPI.h>
 
 char* filepathptr;
@@ -10,22 +9,7 @@ int main()
 {
 	if (FileSelectDialog(fileTypes) != -1)
 	{
-
-		// Get file extension
-		size_t i = filepath.rfind('.', filepath.length());
-		std::string FileExtension = filepath.substr(i + 1, filepath.length() - i);
-
-		if (FileExtension == "alr")
-		{
-			struct stat meta;
-			stat(filepathptr, &meta);
-			std::cout << "The size of " << filepath << " is " << meta.st_size << " bytes.\n\n";
-			ParseAlrFile(filepathptr);
-		}
-		else
-		{
-			ParseDeckFile(filepathptr);
-		}
+		ParseDeckFile(filepathptr);
 
 		std::cout << "\n\nPress any key to exit.\n";
 		char _dummy = _getch();
