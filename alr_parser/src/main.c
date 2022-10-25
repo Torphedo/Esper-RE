631867053bef804b9e4d3fa7e37432078d78573b
Torphedo <73564623+Torphedo@users.noreply.github.com>
2022-10-25 12:02:14 -0400

    Handle empty ALRs
    This stops the tool from generating gigabytes (I got about 12GB) of garbage data.

--- alr_parser/src/main.c
+++ alr_parser/src/main.c
@@ -41,24 +41,27 @@ int main(int argc, char* argv[])
 
 			// Write each large block of data to a separate file. 
 			// size - 1 is because we can't determine the size of the last block easily.
-			for (int i = 0; i < header.pointer_array_size - 1; i++)
+			if (header.pointer_array_size > 0)
 			{
-				// This is fine because there's no user input and the filename is never > 15 characters
-				char filename[15];
-				sprintf(filename, "%u.bin", i);
-				FILE* dump = fopen(filename, "wb");
+				for (int i = 0; i < header.pointer_array_size - 1; i++)
+				{
+					// This is fine because there's no user input and the filename is never > 15 characters
+					char filename[15];
+					sprintf(filename, "%u.bin", i);
+					FILE* dump = fopen(filename, "wb");
 
-				// Length between the current and next pointer
-				size_t size = pointers.pointer_array[i + 1] - pointers.pointer_array[i];
-				char* buffer = malloc(size);
-				if (buffer != NULL) {
-					fseek(file, pointers.pointer_array[i], SEEK_SET);
-					fread(buffer, size, 1, file);
-					fwrite(buffer, size, 1, dump);
-					free(buffer);
+					// Length between the current and next pointer
+					size_t size = pointers.pointer_array[i + 1] - pointers.pointer_array[i];
+					char* buffer = malloc(size);
+					if (buffer != NULL) {
+						fseek(file, pointers.pointer_array[i], SEEK_SET);
+						fread(buffer, size, 1, file);
+						fwrite(buffer, size, 1, dump);
+						free(buffer);
+					}
+
+					fclose(dump);
 				}
-
-				fclose(dump);
 			}
 		}
 		else {
