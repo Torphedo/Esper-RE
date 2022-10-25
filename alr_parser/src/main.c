ed416b8babeb8c4020532dd74acfef446f856776
Torphedo <73564623+Torphedo@users.noreply.github.com>
2022-10-25 11:54:07 -0400

    Support arbitrary filenames

--- alr_parser/src/main.c
+++ alr_parser/src/main.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "data_structures.h"
 
-static const char input_name[] = "bin/pc00a.alr";
+static char* input_name;
 
-int main()
+int main(int argc, char* argv[])
 {
+	// Parse command-line arguments.
+	if (argc > 1)
+	{
+		input_name = malloc(strlen(argv[1]));
+		if (input_name != NULL) {
+			strcpy(input_name, argv[1]);
+		}
+		else {
+			printf("Failed to allocate memory for input filename!\n");
+			exit(0);
+		}
+	}
+	else {
+		printf("Please provide an input fileame.\n");
+		system("pause"); // Windows-only and should be replaced.
+		exit(1);
+	}
+
+
 	FILE* file = fopen(input_name, "rb");
 	if (file != NULL)
 	{
