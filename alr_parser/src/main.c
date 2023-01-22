3ec929e4450d8a07af4c13b44869a6f3b35a5ce6
Torphedo <73564623+Torphedo@users.noreply.github.com>
2023-01-21 23:27:04 -0500

    Massively simplify argument parsing
    I want to maybe do something with hashes here in the future, I think it might be more elegant / make it easier to add new flags

--- alr_parser/src/main.c
+++ alr_parser/src/main.c
@@ -5,65 +5,41 @@
 #include "logging.h"
 #include "parsers.h"
 
-// Cross-platform pause that doesn't wait for a newline.
+// Cross-platform pause
 static inline void pause()
 {
     log_error(INFO, "Press Enter to exit...");
     unsigned char dummy = getchar();
 }
 
-// Thin wrapper around --dump to print out more info
-static bool info_dump(char* filename)
-{
-	set_info_mode();
-	block_parse_all(filename);
-	return true;
-}
-
-// Using a macro so it can be easily iterated over
-#define M_OPERATION_COUNT 3
-
-// Function pointer array for different command-line options
-bool (*operation_funcs[M_OPERATION_COUNT]) (char*) = {
-	split_alr,
-	block_parse_all,
-	info_dump
-};
-static const char* operations[M_OPERATION_COUNT] = { "--split", "--dump", "--info"};
-
-static char* input_name;
-
 int main(int argc, char* argv[])
 {
-	// Parse command-line arguments.
-	if (argc > 1)
-	{
-		input_name = malloc(strlen(argv[1]));
-		if (input_name != NULL) {
-			strcpy(input_name, argv[1]);
-		}
-		else {
-			log_error(CRITICAL, "Failed to allocate memory for input filename!\n");
-			exit(0);
-		}
-		if (argc > 2) {
-			for (unsigned int i = 0; i < M_OPERATION_COUNT; i++)
-			{
-				if (!(strcmp(argv[2], operations[i])))
-				{
-					(*operation_funcs[i]) (input_name);
-				}
-			}
-		}
-		else {
-			(*operation_funcs[0]) (input_name);
-		}
-	}
-	else {
-		log_error(INFO, "Please provide an input filename.\n");
-        pause();
-		return 1;
-	}
+    // Parse command-line arguments.
+    switch (argc)
+    {
+        case 1:
+            log_error(INFO, "Please provide an input filename.\n");
+            pause();
+            return 1;
+        case 2:
+            log_error(WARNING, "No action specified, defaulting to --split\n");
+            split_alr(argv[1]);
+            break;
+        default:
+            if (strcmp(argv[2], "--split") == 0)
+            {
+                split_alr(argv[1]);
+            }
+            else if (strcmp(argv[2], "--dump") == 0)
+            {
+                block_parse_all(argv[1]);
+            }
+            else if (strcmp(argv[2], "--info") == 0)
+            {
+                set_info_mode();
+                block_parse_all(argv[1]);
+            }
+    }
 
 	return 0;
 }
