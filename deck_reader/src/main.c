06b47997ce4d333aa78f17a7b2221cdff74fd2aa
torph <73564623+Torphedo@users.noreply.github.com>
2023-09-06 14:18:01 -0400

    Back to command-line, simpler CMake This makes it run on Linux again (had to remove getch() call and explorer pop-up). The CMake subfolder was just unecessary.

--- deck_reader/src/main.c
+++ deck_reader/src/main.c
@@ -1,77 +1,76 @@
 #include <stdio.h>
-#include <conio.h>
+#include <stdint.h>
 
 #include "winAPI.h"
 #include "skill_names.h"
 
-typedef struct
-{
+typedef struct {
     char Header[4];
     char Name[16];
-    short CardData[30];
-    short SchoolCount;
-    short Metadata; // Purpose unknown. Made up of 2 bytes, which seem to always be 0x00 or 0x40.
-    int MissionClears;
-    int MissionAttempts;
-    int MultiplayerWins;
-    int MultiplayerWinRate;
+    int16_t CardData[30];
+    uint16_t SchoolCount;
+    uint16_t Metadata; // Purpose unknown. Made up of 2 bytes, which seem to always be 0x00 or 0x40.
+    uint32_t MissionClears;
+    uint32_t MissionAttempts;
+    uint32_t MultiplayerWins;
+    uint32_t MultiplayerWinRate;
 }deck_t;
 
-static void PrintDeckFile(deck_t deck)
-{
-	printf("Name: %s\n", deck.Name);
-	printf("School Count: %hi\n", deck.SchoolCount);
-	printf("Unknown Metadata: %hi\n", deck.Metadata);
-	printf("Mission Clears: %i\n", deck.MissionClears);
-	printf("Mission Attempts: %i\n", deck.MissionAttempts);
-	printf("Multiplayer Wins: %i\n", deck.MultiplayerWins);
-	printf("Multiplayer Win Rate: %i%%\n", deck.MultiplayerWinRate);
+static void PrintDeckFile(deck_t deck) {
+    printf("Name: %s\n", deck.Name);
+    printf("School Count: %hi\n", deck.SchoolCount);
+    printf("Unknown Metadata: %hi\n", deck.Metadata);
+    printf("Mission Clears: %i\n", deck.MissionClears);
+    printf("Mission Attempts: %i\n", deck.MissionAttempts);
+    printf("Multiplayer Wins: %i\n", deck.MultiplayerWins);
+    printf("Multiplayer Win Rate: %i%%\n", deck.MultiplayerWinRate);
 
     printf("\n//============================================================================\\\\\n");
 
     static const short card_count = 30;
 
-	for (unsigned int i = 0; i < card_count; i++)
-    {
-        printf("|| Card #%-2d", i + 1);
+    for (unsigned int i = 0; i < card_count; i++) {
+	printf("|| Card #%-2d", i + 1);
 
-		if (deck.CardData[i] == -1) {
+	// ID of -1 is for Aura Particle
+	if (deck.CardData[i] == -1) {
             printf(": %-26s", SkillIDs[0]);
-		}
-		else if (deck.CardData[i] >= 394) // IDs 394 - 499 are all copies of Psycho Wave
-		{
-			printf(": %-26s", SkillIDs[394]);
-		}
-		else {
-            printf(": %-26s", SkillIDs[deck.CardData[i]]);
-		}
-		if (i % 2) { printf("||\n"); }
+	}
+	// IDs 394 - 499 are all copies of Psycho Wave
+	else if (deck.CardData[i] >= 394) {
+	    printf(": %-26s", SkillIDs[394]);
+	}
+	else {
+	    printf(": %-26s", SkillIDs[deck.CardData[i]]);
 	}
 
-	printf("\\\\============================================================================//\n");
+	if (i % 2) { printf("||\n"); }
+    }
+
+    printf("\\\\============================================================================//\n");
 }
 
-int main()
-{
-    char* filepath = FileSelectDialog();
-	if (filepath != NULL)
-	{
-		deck_t deck = { 0 };
+int main(int argc, char** argv) {
+    if (argc == 1) {
+	printf("Please pass in a filename on the command-line or drag-and-drop a file onto the program.\n");
+    }
+    char* filepath = argv[1];
+    if (filepath != NULL) {
+	deck_t deck = {0};
 
-		FILE* deck_file = fopen(filepath, "rb");
-		if (deck_file)
-		{
-			fread(&deck, sizeof(deck_t), 1, deck_file);
-			fclose(deck_file);
-		}
-		PrintDeckFile(deck);
+	FILE* deck_file = fopen(filepath, "rb");
+	if (deck_file) {
+	    fread(&deck, sizeof(deck_t), 1, deck_file);
+	    fclose(deck_file);
+	}
+	PrintDeckFile(deck);
 
-		printf("\n\nPress any key to exit.\n");
-		int _dummy = _getch(); // Pause
-		return 0;
-	}
-	else
-	{
-		return 1;
-	}
+	printf("\n\nPress Enter to exit.\n");
+	char dummy = 0;
+	scanf("%c", &dummy);
+	return 0;
+    }
+    else {
+	return 1;
+    }
 }
