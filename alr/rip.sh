0da60f022239ac13d18ccf61f85776a08373ce80
torph <73564623+Torphedo@users.noreply.github.com>
2023-12-19 23:13:03 -0500

    Add comments, make rip script more intuitive to use Before, you had to manually copy the ALR binary into the bin folder after each rebuild.

--- alr/rip.sh
+++ alr/rip.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# Run this in the "bin/" folder inside of the build output folder, after
-# copying the "alr" binary file into the same folder. See single_rip.sh for
-# more details.
-find . -name "*.alr" | parallel --verbose --max-args 1 ./single_rip.sh {}
+# Run this script in the build folder, with the "alr" binary and # the "bin"
+# folder full of ALR files in the same folder. It will rip textures from all
+# ALR files on all threads using the GNU parallel tool. You might need to
+# install parallel first.
+
+# Go into bin/ folder with all the ALR files
+cd bin
+
+find . -name "*.alr" | parallel --verbose --max-args 1 ../single_rip.sh {}
 
--- alr/single_rip.sh
+++ alr/single_rip.sh
@@ -3,16 +3,19 @@
 # Remove the extension from the file extension.
 filename="${1%.*}"
 
+# Grab alr binary from main build folder & put it into new output folder for
+# each ALR.
 mkdir -p $filename
-
-cp alr $filename
-
+cp ../alr $filename
 cd $filename
 
+# Rip textures.
+pwd
 ./alr --dump "../$1"
 
-# Delete temp files.
+# Delete temp copy of alr binary
 rm alr
 
+# Script is for texture ripping, so the chunk streams are unnecessary
 rm -rf streams
 
