2b24d27aeb54cad1ab386efbd00e68ce4086141b
torph <73564623+Torphedo@users.noreply.github.com>
2023-12-19 21:43:30 -0500

    Add comments for rip script

--- alr/rip.sh
+++ alr/rip.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Run this in the "bin/" folder inside of the build output folder, after
+# copying the "alr" binary file into the same folder. See single_rip.sh for
+# more details.
 find . -name "*.alr" | parallel --verbose --max-args 1 ./single_rip.sh {}
+
