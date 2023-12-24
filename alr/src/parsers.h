43935934c9430eb03c3de2467565e8fcec0737e8
torph <73564623+Torphedo@users.noreply.github.com>
2023-12-23 23:45:36 -0500

    Completely remove "parsers" source file

--- alr/src/parsers.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdbool.h>
-
-#include "arguments.h"
-
-// Parses an ALR 1 chunk at a time. This will output any readable files it can
-// find, such as textures.
-bool chunk_parse_all(char* alr_filename, flags options);
-
