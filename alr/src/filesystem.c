1108c18d7dd518eba36b1956f22985bd41597b67
torph <73564623+Torphedo@users.noreply.github.com>
2023-12-25 01:27:21 -0500

    Stop putting filesystem functions in a header

---
+++ alr/src/filesystem.c
@@ -0,0 +1,52 @@
+#include <malloc.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+
+#include "logging.h"
+#include "int_shorthands.h"
+
+u64 filesize(const char* path) {
+    struct stat st = {0};
+    if (stat(path, &st) != 0) {
+        LOG_MSG(error, "Failed to get file metadata for %s\n", path);
+        return 0;
+    }
+    return st.st_size;
+}
+
+bool file_exists(const char* path) {
+    struct stat st = {0};
+    u32 result = stat(path, &st);
+
+    // File should be a regular file (S_IFREG) and stat() should return success
+    bool is_regular_file = ((st.st_mode & S_IFMT) == S_IFREG);
+    return ((result == 0) && is_regular_file);
+}
+
+bool dir_exists(const char* path) {
+    struct stat st = {0};
+    return (stat(path, &st) == 0 && S_ISDIR(st.st_mode));
+}
+
+u8* file_load(const char* path) {
+    if (!file_exists(path)) {
+        LOG_MSG(error, "Requested file %s does not exist.\n", path);
+        return NULL;
+    }
+    u32 size = filesize(path);
+    u8* buffer = calloc(1, size);
+    FILE* resource = fopen(path, "rb");
+
+    // Clean up & return NULL if anything goes wrong
+    if (resource == NULL || buffer == NULL) {
+        fclose(resource);
+        free(buffer);
+        return NULL;
+    }
+
+    // Read the entire file and return its contents
+    fread(buffer, size, 1, resource);
+    fclose(resource);
+    return buffer;
+}
+
--- alr/src/filesystem.h
+++ alr/src/filesystem.h
@@ -1,20 +1,15 @@
+#pragma once
 #include <stdbool.h>
-#include <sys/stat.h>
 
-#include "logging.h"
 #include "int_shorthands.h"
 
-static u64 filesize(const char* path) {
-    struct stat st = {0};
-    if (stat(path, &st) != 0) {
-        LOG_MSG(error, "Failed to get file metadata for %s\n", path);
-        return 0;
-    }
-    return st.st_size;
-}
+u64 filesize(const char* path);
 
-static bool dir_exists(const char* path) {
-    struct stat st = {0};
-    return (stat(path, &st) == 0 && S_ISDIR(st.st_mode));
-}
+bool file_exists(const char* path);
+
+bool dir_exists(const char* path);
+
+// Load an entire file into a buffer. Caller is responsible for freeing buffer,
+// returns NULL on error.
+u8* file_load(const char* path);
 
