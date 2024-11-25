f1ac8f5a4a5fd95769a9c5c53e574b846e1a5112
torph <73564623+Torphedo@users.noreply.github.com>
2024-11-25 02:30:02 -0500

    Add ALR splitting

--- polaris/src/polaris_gui.cxx
+++ polaris/src/polaris_gui.cxx
@@ -9,6 +9,7 @@ extern "C" {
     #include <GLFW/glfw3.h>
     #include <common/image.h>
     #include <common/file.h>
+    #include <common/vfile.h>
     #include <common/logging.h>
     #include <common/gl/input.h>
     #include "viewer/render_image.h"
@@ -24,21 +25,44 @@ bool polaris::do_gui(GLFWwindow* window) {
     }
 
     ImGui::Begin("ALR Select");
+    if (ImGui::Button("Load ALR")) {
+        ImGuiFileDialog::Instance()->OpenDialog("chooseALR", "Choose ALR File", ".alr", {});
+    }
+
+    // Display file dialog if appropriate
+    if (ImGuiFileDialog::Instance()->Display("chooseALR")) {
+        // If user cancels, we can't load anything
+        if (ImGuiFileDialog::Instance()->IsOk()) {
+            const std::string path = ImGuiFileDialog::Instance()->GetFilePathName();
+
+            // Load the texture
+            this->alr_data = file_load(path.c_str());
+            if (this->alr_data != nullptr) {
+                this->alr_size = file_size(path.c_str());
+                this->chunks = shatter_alr(this->alr_data, this->alr_size);
+            }
+        }
+
+        // Close the dialog
+        ImGuiFileDialog::Instance()->Close();
+    }
 
     static s32 selected = 0;
-    ImGui::BeginListBox(" ");
-    for (int n = 0; n < 55; n++) {
-        char buf[32];
-        sprintf(buf, "Object %d", n);
-        const bool is_selected = (selected == n);
-        if (ImGui::Selectable(buf, is_selected)) {
-            selected = n;
+    if (ImGui::BeginListBox(" ", ImVec2(0, -FLT_MIN))) {
+        for (size_t n = 0; n < this->chunks.size(); n++) {
+            chunk_desc chunk = this->chunks.at(n);
+            char buf[128];
+            sprintf(buf, "0x%x chunk @ 0x%lx [%d bytes] ##%d", chunk.id, chunk.offset, chunk.size, n);
+            const bool is_selected = (selected == n);
+            if (ImGui::Selectable(buf, is_selected)) {
+                selected = n;
+            }
+            if (is_selected) {
+                ImGui::SetItemDefaultFocus();
+            }
         }
-        if (is_selected) {
-            ImGui::SetItemDefaultFocus();
-        }
+        ImGui::EndListBox();
     }
-    ImGui::EndListBox();
     ImGui::End();
 
     ImGui::Begin("Texture Select");
@@ -56,7 +80,7 @@ bool polaris::do_gui(GLFWwindow* window) {
             const s64 size = 32 * 1024 * 1024;
             u8* buf = (u8*)calloc(1, size);
             memset(buf, 0xCC, size);
-            if (buf != NULL && file_exists(path.c_str())) {
+            if (buf != nullptr && file_exists(path.c_str())) {
                 // The buffer pointer we just allocated is copied into the context
                 const texture tex = image_buf_load(path.c_str(), buf, size);
                 this->img_ctx = image_init(tex);
@@ -73,9 +97,9 @@ bool polaris::do_gui(GLFWwindow* window) {
     if (this->img_ctx.img.width == 0 || this->img_ctx.img.height == 0) {
         ratio = 1.0f; 
     }
-    camera_update(NULL, ratio);
+    camera_update(nullptr, ratio);
 
-    if (this->img_ctx.img.data != NULL) {
+    if (this->img_ctx.img.data != nullptr) {
         image_render(&this->img_ctx, window);
 
         // Manages active texture's format, dimensions, etc.
@@ -87,4 +111,34 @@ bool polaris::do_gui(GLFWwindow* window) {
     // It's the end of the frame for us, save the current input
     this->prev_input = input;
     return true;
+}
+
+std::vector<chunk_desc> polaris::shatter_alr(const u8* buf, s64 size) {
+    // Technically we cast away const here, but we don't write any data so it's
+    // fine.
+    vfile vf = vfile_open((void*)buf, size);
+    std::vector<chunk_desc> out;
+
+    // Loop until we exhaust the buffer or exit early
+    u32 prev_id = -1;
+    while (!vfile_eof(vf)) {
+        // Read chunk data. We have to copy it over 1 field at a time because we
+        // don't actually want/need any more of the chunk data.
+        chunk_desc chunk = {.offset = vf.pos};
+        chunk.id = VFILE_READ(u32, &vf);
+        chunk.size = VFILE_READ(s32, &vf);
+
+        if (chunk.id == 0 && prev_id == 0) {
+            // There's never multiple consecutive chunks with ID 0. This means
+            // we've hit an empty area, probably the end of the chunk data.
+            break;
+        }
+
+        // Advance to the next chunk & add to output
+        vf.pos = chunk.offset + chunk.size;
+        out.push_back(chunk);
+        prev_id = chunk.id; // Save current ID
+    }
+
+    return out;
 }
--- polaris/src/polaris_gui.hxx
+++ polaris/src/polaris_gui.hxx
@@ -1,19 +1,35 @@
 #pragma once
-#include <GLFW/glfw3.h>
+#include <vector>
 
 extern "C" {
+    #include <GLFW/glfw3.h>
     #include "common/gl/input.h"
     #include "viewer/render_image.h"
 }
+
+typedef struct {
+    u32 id;
+    s32 size; // Sizes can actually be signed, not sure why.
+    uintptr_t offset;
+
+    // A chunk is "orphaned" if it can't be found using the offset table
+    bool orphan;
+}chunk_desc;
 
 struct polaris {
 public:
     input_internal prev_input;
     img_state img_ctx;
+    u8* alr_data;
+    s64 alr_size;
+    std::vector<chunk_desc> chunks;
 
     /// @brief Effectively the "real" entry point for Polaris, driving the UI
     ///
     /// @param window The window, needed for aspect ratio and input and such
     /// @return Returns false when the user wants to exit (if ever)
     bool do_gui(GLFWwindow* window);
+
+    /// @brief "Shatter" an ALR into all its chunks
+    static std::vector<chunk_desc> shatter_alr(const u8* buf, s64 size);
 };
