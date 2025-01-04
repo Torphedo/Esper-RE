904d9dc669afa68f360215523dd8989e66630d16
Torphedo <73564623+Torphedo@users.noreply.github.com>
2025-01-04 15:20:07 -0500

    Fix text decoder bounds checking
    Also changed the loop to be easier for the compiler to unroll

--- formats/pd_common.c
+++ formats/pd_common.c
@@ -1,11 +1,13 @@
 #include "pd_common.h"
-const char char_lookup[] = "0123456789abcdefghijklmnopqrstuvwxyz_";
+// The first character is NUL, so that an index of 0 or sizeof(char_lookup) - 1
+// gives a NUL. This lets us clamp the value instead of doing a bounds check.
+const char char_lookup[] = "\0000123456789abcdefghijklmnopqrstuvwxyz_";
 
 // Decode 6 characters from the 32-bit integer they're stored in
 void decode_single32(char* output, u32 val) {
-    // This is signed so we can double-check it didn't overflow somehow
-    s8 cur_char_idx = (PD_ENCODED_CHAR_COUNT / 2) - 1;
-    while (val > 0) {
+    // We use a signed loop counter so we can check for underflows.
+    // The loop counter represents the position of the character being decoded.
+    for (s32 i = (PD_ENCODED_CHAR_COUNT / 2) - 1; i >= 0; i--) {
         // Each character code is multiplied by 40 and added to the encoded
         // value to store 6 characters in 4 bytes. To decode, we find the
         // remainder at each power of 40, and that's our character code.
@@ -13,16 +15,12 @@ void decode_single32(char* output, u32 val) {
         val -= remainder;
         val /= 40;
 
-        if (cur_char_idx < 0) {
-            // We'll probably never hit this, but just in case...
-            break;
-        }
-
         // The value stored per character is actually an index into a lookup
         // table. (This is how the game does it so we're copying them)
-        output[cur_char_idx] = char_lookup[remainder - 1];
-        cur_char_idx--;
+
+        // Clamping to sizeof() - 1 instead of strlen() - 1 is intentional. This
+        // means that an out-of-bounds remainder will get a NUL character.
+        const u8 idx = CLAMP(0, remainder, sizeof(char_lookup) - 1);
+        output[i] = char_lookup[idx]; // Store decoded character
     }
-
 }
-
