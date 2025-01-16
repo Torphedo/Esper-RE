96d251f947ac3e9db5c0ea0e3891a2e290794361
torph <73564623+Torphedo@users.noreply.github.com>
2025-01-16 11:26:10 -0500

    Re-organize file structure No changes to functionality.

--- polaris.cmake
+++ polaris.cmake
@@ -1,23 +1,23 @@
 cmake_minimum_required(VERSION 3.13)
 project(polaris
-    VERSION 0.0.1
+    VERSION 1.0.0
     LANGUAGES CXX C
 )
 set(CMAKE_CXX_STANDARD 17)
 
 # Add ImGui as a library
 add_library(imgui STATIC
-    ../ext/imgui/imgui.cpp
-    ../ext/imgui/imgui_demo.cpp
-    ../ext/imgui/imgui_draw.cpp
-    ../ext/imgui/imgui_tables.cpp
-    ../ext/imgui/imgui_widgets.cpp
-    ../ext/imgui/backends/imgui_impl_opengl3.cpp
-    ../ext/imgui/backends/imgui_impl_glfw.cpp
+    ext/imgui/imgui.cpp
+    ext/imgui/imgui_demo.cpp
+    ext/imgui/imgui_draw.cpp
+    ext/imgui/imgui_tables.cpp
+    ext/imgui/imgui_widgets.cpp
+    ext/imgui/backends/imgui_impl_opengl3.cpp
+    ext/imgui/backends/imgui_impl_glfw.cpp
 )
 
 # Internal build tool to generate headers for GLSL shaders
-add_executable(txt2h "../ext/txt2h.c")
+add_executable(txt2h "ext/txt2h.c")
 
 # Autogenerate string constant headers for each GLSL source file using txt2h.c
 set(glsl_headers)
@@ -35,15 +35,15 @@ foreach(glsl_src ${GLSL_SOURCES})
 endforeach()
 
 add_executable(polaris
-    src/main.cxx
-    src/gui_loop.cxx
-    src/alr_texture.cxx
-    src/polaris.cxx
-    src/file_pickers.cxx
-    src/imgui_utils.cxx
+    src/polaris/main.cxx
+    src/polaris/gui_loop.cxx
+    src/polaris/alr_texture.cxx
+    src/polaris/polaris.cxx
+    src/polaris/file_pickers.cxx
+    src/polaris/imgui_utils.cxx
 
-    ../formats/pd_common.c
-    ../ext/glad/src/glad.c
+    src/formats/pd_common.c
+    ext/glad/src/glad.c
 
     # Adding the GLSL headers here auto-generates them during the build
     ${glsl_headers}
