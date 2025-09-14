08169bcfc76007b089d7ab20450c1a7f30762555
torph <73564623+Torphedo@users.noreply.github.com>
2025-09-14 15:09:35 -0400

    Increase far plane to see the skybox

--- src/polaris/camera.hxx
+++ src/polaris/camera.hxx
@@ -19,7 +19,7 @@ struct camera {
     float move_speed = 15.0f;
     float mouse_sens = 0.015f;
     float near_clip_plane = 0.1f;
-    float far_clip_plane = 5000.0f;
+    float far_clip_plane = 10000.0f;
 
     bool invert_mouse_x = false;
     bool invert_mouse_y = false;
