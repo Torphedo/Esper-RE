905fb42ab8cf39bd7491ea74b27c1fc3bda51061
torph <73564623+Torphedo@users.noreply.github.com>
2025-11-02 01:46:21 -0500

    Simplify some math, triangle strip fix

--- src/polaris/selector_ray.cxx
+++ src/polaris/selector_ray.cxx
@@ -41,10 +41,8 @@ bool raycast_aabb(ray_t ray, vec3s min, vec3s max) {
 }
 
 vec3s vec3_transform(vec3s input, mat4s xform) {
-    vec4s v = {input.x, input.y, input.z, 1.0f};
-    v = glms_mat4_mulv(xform, v);
-    const vec3s output = {v.x, v.y, v.z};
-    return output;
+    const vec4s v = glms_mat4_mulv(xform, glms_vec4(input, 1.0f));
+    return glms_vec3(v);
 }
 
 bool raycast(ray_t ray, const u8* vertbuf, u32 vertex_size, mat4s transform, const idxbuf_header* idxbuf) {
@@ -57,10 +55,18 @@ bool raycast(ray_t ray, const u8* vertbuf, u32 vertex_size, mat4s transform, con
 
     const u16* indices = (u16*)&idxbuf[1]; // Indices begin when header ends
     const bool strip = idxbuf->primitive_type == IDX_TYPE_STRIP;
-    for (u32 i = 0; i < idxbuf->num_indices; i++) {
-        const u16 idx1 = indices[i];
-        const u16 idx2 = indices[i + 1];
-        const u16 idx3 = indices[i + 2];
+    // Copied from alr_dump.cxx!dump_idx_buf()
+    for (s32 i = 2; i < idxbuf->num_indices; i++) {
+        const u16 idx1 = indices[i - 2];
+        const u16 idx2 = indices[i - 1];
+        const u16 idx3 = indices[i];
+        if (idx1 == idx2 || idx1 == idx3 || idx2 == idx3) {
+            // Triangle strips will repeat 1 index to create a triangle with an
+            // area of 0, which is used to end a strip and start another.
+            // We skip these since they're not part of the geometry.
+            continue;
+        }
+
         const u8* vert1 = vertbuf + (idx1 * vertex_size);
         const u8* vert2 = vertbuf + (idx2 * vertex_size);
         const u8* vert3 = vertbuf + (idx3 * vertex_size);
