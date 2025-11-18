2d734c95a4ea6ef4c43a31f05194b3b60cb4d0fc
torph <73564623+Torphedo@users.noreply.github.com>
2025-11-18 01:29:33 -0500

    Add some new STH2 research

--- src/formats/sth2.h
+++ src/formats/sth2.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <common/int.h>
+#include <assert.h>
 
 typedef struct {
     u32 magic;
@@ -14,6 +15,46 @@ typedef struct {
 
     u32 unk5[5];
 }sth2_header;
+static_assert(sizeof(sth2_header) == 0x40, "Wrong STH2 header size!");
+
+typedef struct {
+    u32 magic;
+    u32 unknown;
+    u32 num_offsets;
+    u32 pad;
+
+    s32 offsets[];
+}sth2_real_header;
+static_assert(sizeof(sth2_real_header) == 0x10, "Wrong STH2 'REAL' header size!");
+
+typedef struct {
+    u32 magic;
+    u32 size;
+    u32 num_entries;
+    u32 unk1;
+    u32 unk2;
+    u32 unk3;
+    u8 pad[0x18];
+}trat_header;
+static_assert(sizeof(trat_header) == 0x30, "Wrong STH2 'TRAT' header size!");
+
+typedef struct {
+    u32 magic;
+    u8 unk5[0xFC];
+}evnt_header;
+static_assert(sizeof(evnt_header) == 0x100, "Wrong STH2 'EVNT' header size!");
+
+typedef struct {
+    u32 magic;
+    u32 size;
+    u32 num_entries;
+    u32 pad1;
+    u32 unk1;
+    u32 unk2;
+    u8 pad2[0x18];
+    u32 offsets[];
+}reat_header;
+static_assert(sizeof(reat_header) == 0x30, "Wrong STH2 'REAT' header size!");
 
 typedef struct {
     u32 magic;
@@ -22,6 +63,7 @@ typedef struct {
     u32 unk2;
     u32 offsets[];
 }sth2_wave_header;
+static_assert(sizeof(sth2_wave_header) == 0x10, "Wrong STH2 'WAVE' header size!");
 
 enum {
     PD_SAMPLE_RATE_UWP = 22050,
