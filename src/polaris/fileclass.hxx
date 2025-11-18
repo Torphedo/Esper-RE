757cb70e3889c68ffda6c3a727baa0a3cfa395ff
Torphedo <73564623+Torphedo@users.noreply.github.com>
2025-11-18 18:52:03 -0500

    Add default implementation of fileclass::load_verify()

--- src/polaris/fileclass.hxx
+++ src/polaris/fileclass.hxx
@@ -6,7 +6,9 @@ struct fileclass {
     u32 size = 0;
 
     bool load(const char* path) noexcept;
-    virtual bool load_verify() const noexcept = 0;
+    virtual bool load_verify() const noexcept {
+        return true;
+    }
 
     void unload() noexcept;
     bool save(const char* path) const noexcept;
