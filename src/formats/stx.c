0f1ce62c6214e93b79da6173ed05120bd992a8df
Torphedo <73564623+Torphedo@users.noreply.github.com>
2025-12-22 18:35:04 -0500

    Add current STX research results

---
+++ src/formats/stx.c
@@ -0,0 +1,25 @@
+#include "stx.h"
+
+stx_block_header stx_block_create(u16 total_num_blocks, u16 idx) {
+    const bool is_first_block = (idx == 0);
+    const u16 offset = (is_first_block) ? STX_FIRST_OFFSET : sizeof(stx_block_header);
+    stx_block_header out = {
+        .magic = STX_MAGIC,
+        .offset = {
+            .next_block = offset,
+        },
+        .channel_count = 2,
+        .build_day = STX_PC_BUILD_DAY,
+        .build_month = STX_PC_BUILD_MONTH,
+        .build_year = STX_PC_BUILD_YEAR,
+        .block_count = total_num_blocks,
+        .block_idx = idx,
+        // In vanilla files, the first block has this set to 0.
+        .channel_size = STX_TOTAL_BLOCK_SAMPLES,
+        .block_number = (u16)(idx + 1),
+        .loop_start_block = 1,
+        .loop_end_block = (u16) MAX(0, total_num_blocks - 1),
+    };
+
+    return out;
+}
--- src/formats/stx.h
+++ src/formats/stx.h
@@ -4,12 +4,30 @@
 // However, their information is a bit scattered and they don't have any usable
 // C structures.
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <common/int.h>
 #include <common/file.h>
+#include "sth2.h"
 
 enum {
     STX_MAGIC = MAGIC('S', 'T', 'H', 'D'),
     STX_MAX_CHANNELS = 8,
+    STX_CHANNEL_NAME_SIZE = 0x20,
+    STX_PC_SAMPLE_RATE = PD_SAMPLE_RATE_UWP * 2,
+
+    // Use these if you want to be as close as possible to the PC files
+    STX_PC_BUILD_DAY = 0x23,
+    STX_PC_BUILD_MONTH = 0x03,
+    STX_PC_BUILD_YEAR = 0x2017,
+
+    // Number of samples per channel per block.
+    STX_BLOCK_SAMPLES = 504,
+
+    // The total number of samples per block, assuming 2 channels.
+    STX_TOTAL_BLOCK_SAMPLES = STX_BLOCK_SAMPLES * 2,
 };
 
 // All offsets are relative.
@@ -34,7 +52,7 @@ typedef struct {
     u16 block_count; // Total block count of the file
     u16 block_idx;
     u16 pad;
-    u16 channel_size; // In bytes, per channel (so double to get full size)
+    u16 channel_size; // Measured in samples
     u16 block_number; // Always block_idx + 1, probably for convenience.
     u16 loop_start_block;
     u16 loop_end_block;
@@ -44,10 +62,9 @@ typedef struct {
 // After the first (header) block, there are 8 of this structure.
 typedef struct {
     u32 sample_rate; // Measured in Hz
-    // Not always set, speculated to be volume as a percentage of UINT16_MAX
-    // Labelled as both "volume" and "pan" in vgmstream comments
-    u16 volume1;
-    u16 volume2;
+    u8 volume;
+    // First byte is left side volume, second is right side.
+    s8 pan[2];
 
     // Makes structure 0x40 bytes. Could also be more fields that we've only
     // seen set to 0.
@@ -59,6 +76,38 @@ typedef struct {
 typedef struct {
     stx_block_header header;
     stx_channel channels[STX_MAX_CHANNELS];
-    unsigned char channel_names[STX_MAX_CHANNELS][0x20];
+    unsigned char channel_names[STX_MAX_CHANNELS][STX_CHANNEL_NAME_SIZE];
 }stx_first_block;
 
+// This is separate because we need struct sizes
+enum {
+    STX_BLOCK_SIZE = sizeof(stx_block_header) + STX_TOTAL_BLOCK_SAMPLES * sizeof(u16),
+    STX_FIRST_OFFSET = STX_BLOCK_SIZE,
+};
+
+/// @brief Generate an STX block header
+///
+/// The first block in the file gets special treatment in some fields, so make
+/// sure to pass a block index of 0 to get a valid header.
+/// @param total_num_blocks Total number of STX blocks in this file
+/// @param idx Index of this block
+/// @return Block header data
+stx_block_header stx_block_create(u16 total_num_blocks, u16 idx);
+
+/// @brief Calculate the number of blocks in the STX file
+/// @param total_samples The total number of samples across all channels
+static u32 stx_num_blocks_from_samples(u32 total_samples) {
+    const u32 num_blocks = ALIGN_UP(total_samples, STX_TOTAL_BLOCK_SAMPLES) / STX_TOTAL_BLOCK_SAMPLES;
+    return num_blocks;
+}
+
+/// @brief Calculate the expected size of an STX file
+/// @param total_samples The total number of samples across all channels
+static s64 stx_size_from_sample_count(u32 total_samples) {
+    const u32 num_blocks = stx_num_blocks_from_samples(total_samples);
+    return STX_FIRST_OFFSET + STX_BLOCK_SIZE * num_blocks;
+}
+
+#ifdef __cplusplus
+}
+#endif
