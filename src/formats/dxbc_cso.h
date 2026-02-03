0898c42e0ea7982e3f3192f455ed51f86f8d09e5
torph <73564623+Torphedo@users.noreply.github.com>
2026-02-03 02:29:42 -0500

    Add CSO resource definition viewing

--- src/formats/dxbc_cso.h
+++ src/formats/dxbc_cso.h
@@ -6,6 +6,63 @@
 // https://llvm.org/docs/DirectX/DXContainer.html
 #include <common/int.h>
 
+// https://learn.microsoft.com/en-us/windows/win32/api/d3dcommon/ne-d3dcommon-d3d_shader_cbuffer_flags
+typedef enum {
+    D3D_CBF_USERPACKED = 1,
+    D3D10_CBF_USERPACKED,
+    D3D_CBF_FORCE_DWORD = 0x7fffffff
+}D3D_SHADER_CBUFFER_FLAGS;
+
+// https://learn.microsoft.com/en-us/previous-versions/windows/desktop/legacy/ff476097(v=vs.85)
+typedef enum {
+    D3D11_CT_CBUFFER,
+    D3D11_CT_TBUFFER,
+    D3D11_CT_INTERFACE_POINTERS,
+    D3D11_CT_RESOURCE_BIND_INFO,
+}D3D11_CBUFFER_TYPE;
+
+typedef enum {
+    D3D_SVC_SCALAR = 0,
+    D3D_SVC_VECTOR,
+    D3D_SVC_MATRIX_ROWS,
+    D3D_SVC_MATRIX_COLUMNS,
+    D3D_SVC_OBJECT,
+    D3D_SVC_STRUCT,
+    D3D_SVC_INTERFACE_CLASS,
+    D3D_SVC_INTERFACE_POINTER,
+    D3D10_SVC_SCALAR,
+    D3D10_SVC_VECTOR,
+    D3D10_SVC_MATRIX_ROWS,
+    D3D10_SVC_MATRIX_COLUMNS,
+    D3D10_SVC_OBJECT,
+    D3D10_SVC_STRUCT,
+    D3D11_SVC_INTERFACE_CLASS,
+    D3D11_SVC_INTERFACE_POINTER,
+    D3D_SVC_FORCE_DWORD = 0x7fffffff
+}D3D_SHADER_VARIABLE_CLASS;
+
+typedef enum {
+    D3D_SVT_VOID = 0,
+    D3D_SVT_BOOL = 1,
+    D3D_SVT_INT = 2,
+    D3D_SVT_FLOAT = 3,
+    D3D_SVT_STRING = 4,
+    D3D_SVT_TEXTURE = 5,
+    D3D_SVT_TEXTURE1D = 6,
+    D3D_SVT_TEXTURE2D = 7,
+    /* snip... */
+    D3D_SVT_UINT = 19,
+    D3D_SVT_UINT8 = 20,
+    /* snip... */
+    D3D_SVT_BUFFER = 25,
+    D3D_SVT_CBUFFER = 26,
+    D3D_SVT_TBUFFER = 27,
+    /* snip... */
+    D3D_SVT_DOUBLE = 39,
+    /* snip... */
+    D3D_SVT_FORCE_DWORD = 0x7fffffff
+}D3D_SHADER_VARIABLE_TYPE;
+
 typedef struct {
     u8 Magic[4]; // "DXBC"
     u8 Digest[16];
@@ -34,6 +91,64 @@ typedef struct {
     u32 Unknown;
     ISGNElement elements[];
 }ISGNPart;
+
+// RDEF == "Resource Definition"
+typedef struct {
+    u32 ConstBufferCount;
+    u32 ConstBufferOffset;
+    u32 ResBindingCount;
+    u32 ResBindingOffset;
+    u8 VersionMinor;
+    u8 VersionMajor;
+    u16 ProgramType;
+    u32 Flags; // 256 == NoPreshader
+    u32 CreatorNameOffset;
+}RDEFPart;
+
+typedef struct {
+    u32 NameOffset;
+    u32 VariableCount;
+    u32 VariablesOffset;
+    u32 BufferSize;
+    u32 Flags; // D3D_SHADER_CBUFFER_FLAGS
+    u32 BufferType; // D3D11_CBUFFER_TYPE
+}RDEFConstBuffer;
+
+typedef struct {
+    u32 NameOffset; // Offset from start of part
+    u32 BufferOffset; // Offset of the variable within the buffer
+    u32 Size; // Size of variable
+    u32 Flags; // D3D10_SHADER_VARIABLE_FLAGS (2 == used in the shader)
+    u32 TypeOffset; // Offset from start of part to variable type
+    u32 DefaultValOffset; // 0 means no default value
+}RDEFVariable;
+
+typedef struct {
+    u16 Class; // D3D10_SHADER_VARIABLE_CLASS (3 == column-major matrix)
+    u16 Type; // D3D10_SHADER_VARIABLE_TYPE (3 == float)
+
+    // For matrices
+    u16 MatrixRows;
+    u16 MatrixCols;
+
+    // For arrays
+    u16 ArrayElements;
+
+    // For structs
+    u16 StructMemberCount;
+    u16 FirstMemberOffset; // From start of part
+}RDEFVariableType;
+
+typedef struct {
+    u32 NameOffset; // Offset from start of part
+    u32 InputType; // 0 == cbuffer
+    u32 ResReturnType; // 0 == N/A
+    u32 ResViewDimension; // 0 == N/A
+    u32 SampleNum;
+    u32 BindPoint;
+    u32 BindCount;
+    u32 InputFlags;
+}RDEFResDesc;
 
 typedef struct {
     char Name[4];
--- src/polaris/gui/tool_cso.cxx
+++ src/polaris/gui/tool_cso.cxx
@@ -35,8 +35,8 @@ swizzle_t mask_to_swizzle(u8 val) {
     return out;
 }
 
-void type_text(u8 type) {
-    if (type == 3) {
+void type_text(u16 type) {
+    if (type == D3D_SVT_FLOAT) {
         ImGui::Text("float");
         return;
     }
@@ -44,7 +44,17 @@ void type_text(u8 type) {
     ImGui::Text("%d", type);
 }
 
+void class_text(u16 c) {
+    if (c == D3D_SVC_VECTOR) {
+        ImGui::Text("vector");
+        return;
+    }
+
+    ImGui::Text("%d", c);
+}
+
 void view_isgn(const ISGNPart* part) {
+    ImGui::ScopedIndent indent(ImGui::CharWidth(4));
     for (u32 i = 0; i < part->ElementCount; i++) {
         const ISGNElement& e = part->elements[i];
         const char* name = (const char*)(uintptr_t(part) + e.NameOffset);
@@ -55,8 +65,56 @@ void view_isgn(const ISGNPart* part) {
         ImGui::Text("\tRegister #%d", e.Register);
 
         ImGui::Text("\tComponent Type: ");
-        ImGui::SameLine();
-        type_text(e.ComponentType);
+        ImGui::SameLine(); type_text(e.ComponentType);
+        ImGui::Text("\n");
+    }
+}
+
+void view_rdef(const RDEFPart* part) {
+    const auto start = (uintptr_t)part;
+    const char* creator = (const char*)(start + part->CreatorNameOffset);
+    const auto* buffers = (RDEFConstBuffer*)(start + part->ConstBufferOffset);
+    const auto* resDescs = (RDEFResDesc*)(start + part->ResBindingOffset);
+
+    ImGui::ScopedIndent indent(ImGui::CharWidth(4));
+    ImGui::Text("v%d.%d, created by '%s'", part->VersionMajor, part->VersionMinor, creator);
+    ImGui::Spacing();
+    ImGui::Text("\nConstant Buffers:\n");
+    for (u32 i = 0; i < part->ConstBufferCount; i++) {
+        const RDEFConstBuffer& buf = buffers[i];
+        const char* name = (const char*)(start + buf.NameOffset);
+        const auto* vars = (RDEFVariable*)(start + buf.VariablesOffset);
+
+        ImGui::Separator();
+        ImGui::Text("Buffer '%s' is %d bytes, %d variables", name, buf.BufferSize, buf.VariableCount);
+        ImGui::Text("Flags %d, type %d", buf.Flags, buf.BufferType);
+        ImGui::Text("Variables:");
+
+        for (u32 j = 0; j < buf.VariableCount; j++) {
+            const RDEFVariable& v = vars[j];
+            const char* var_name = (const char*)(start + v.NameOffset);
+            const auto* type = (RDEFVariableType*)(start + v.TypeOffset);
+
+            ImGui::ScopedIndent indent2(ImGui::CharWidth(4));
+            ImGui::Text("'%s' has size %d, type ", var_name, v.Size);
+
+            ImGui::SameLine(); type_text(type->Type);
+
+            ImGui::SameLine(); ImGui::Text(" class ");
+            ImGui::SameLine(); class_text(type->Class);
+        }
+    }
+
+    ImGui::Text("\nResource Bindings:\n");
+    for (u32 i = 0; i < part->ResBindingCount; i++) {
+        const RDEFResDesc& res = resDescs[i];
+        const char* name = (const char*)(start + res.NameOffset);
+
+        ImGui::Separator();
+        ImGui::Text("'%s', bind count %d, bind point %d", name, res.BindCount, res.BindPoint);
+        ImGui::Text("Input flags %d, type %d", res.InputFlags, res.InputType);
+        ImGui::Text("Return type %d, view dimension %d", res.ResReturnType, res.ResViewDimension);
+        ImGui::Text("Sample count %d", res.SampleNum);
     }
 }
 
@@ -67,10 +125,16 @@ void view_cso(const void* data, u32 size) {
     for (u32 i = 0; i < header->PartCount; i++) {
         vf.pos = header->PartOffsets[i];
         const CSOPart* part = VFILE_READ_PTR(CSOPart, &vf);
-        const ISGNPart* isgn = VFILE_READ_PTR(ISGNPart, &vf);
         if (strncmp(part->Name, "ISGN", 4) == 0) {
+            const ISGNPart* isgn = VFILE_READ_PTR(ISGNPart, &vf);
             if (ImGui::CollapsingHeader("Input Elements")) {
                 view_isgn(isgn);
+            }
+        }
+        if (strncmp(part->Name, "RDEF", 4) == 0) {
+            const RDEFPart* rdef = VFILE_READ_PTR(RDEFPart, &vf);
+            if (ImGui::CollapsingHeader("Resource Definitions")) {
+                view_rdef(rdef);
             }
         }
     }
