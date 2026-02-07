fe8ba407bd19a3c8b95d07a5b801da894a61a2cf
torph <73564623+Torphedo@users.noreply.github.com>
2026-02-06 20:55:53 -0500

    Implement skinning in SWBOS shader

--- src/polaris/shaders/skinning.vert.h
+++ src/polaris/shaders/skinning.vert.h
@@ -4,6 +4,8 @@ layout (location = 0) in vec3 a_pos;
 layout (location = 1) in vec2 a_texcoord;
 layout (location = 2) in vec2 a_lightmap_uv;
 layout (location = 3) in vec3 a_normal;
+layout (location = 4) in vec2 a_blendindices;
+layout (location = 5) in vec2 a_blendweights;
 
 uniform mat4 skin_xforms[256];
 uniform mat4 pvm;
@@ -13,11 +15,16 @@ out vec2 lightmap_uv;
 out vec3 normal;
 
 void main() {
-    gl_Position = pvm * skin_xforms[0] * vec4(a_pos, 1.0);
-
     // We map the large integer value into the [0, 1] range for texture lookups
     texcoord = a_texcoord / uv_divisor;
+    vec2 weights = a_blendweights / uv_divisor;
     lightmap_uv = a_lightmap_uv / uv_divisor;
     normal = a_normal / uv_divisor;
+
+    mat4 skin1 = skin_xforms[int(a_blendindices.x)] * weights.x;
+    mat4 skin2 = skin_xforms[int(a_blendindices.y)] * weights.y;
+    mat4 skin = skin2 * skin1;
+
+    gl_Position = pvm * skin * vec4(a_pos, 1.0);
 }
 )";
