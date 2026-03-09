47752df013bf55e931c1b3c188aee02ea2cfe7b6
torph <73564623+Torphedo@users.noreply.github.com>
2026-03-09 00:02:47 -0400

    Fully remove pd_common dependency on bobtail

--- src/formats/sth2.c
+++ src/formats/sth2.c
@@ -2,9 +2,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-#include <common/vfile.h>
-
-#include <formats/wav.h>
+#include "wav.h"
 
 bool extract_sth2(const u8* data, u32 size, const char* outpath, u32 sample_rate) {
     FILE* f = fopen(outpath, "wb");
@@ -13,17 +11,17 @@ bool extract_sth2(const u8* data, u32 size, const char* outpath, u32 sample_rate
         return false;
     }
 
-    vfile vf = vfile_open((u8*)data, size);
-    sth2_header header = VFILE_READ(sth2_header, &vf);
+    u32 pos = 0;
+    const sth2_header* header = (const sth2_header*)data;
+    pos = header->wave_offset;
 
-    vf.pos = header.wave_offset;
-    const sth2_wave_header pd_wave_header = VFILE_READ(sth2_wave_header, &vf);
-    const u32 offsets_size = pd_wave_header.num_offsets * sizeof(*pd_wave_header.offsets);
-    vfile_seek(&vf, offsets_size);
+    const sth2_wave_header* pd_wave_header = (sth2_wave_header*)(data + pos);
+    const u32 offsets_size = pd_wave_header->num_offsets * sizeof(*pd_wave_header->offsets);
+    pos += offsets_size;
 
-    const u32 header_size = sizeof(pd_wave_header) + offsets_size;
-    const u32 audio_size = pd_wave_header.size - header_size;
-    const u8* audio = vfile_cur(vf);
+    const u8* audio = (data + pos);
+    const u32 header_size = sizeof(*pd_wave_header) + offsets_size;
+    const u32 audio_size = pd_wave_header->size - header_size;
 
     wav_write_audio(sample_rate, 1, sizeof(u16), WAV_FMT_PCM, audio, audio_size, f);
     fclose(f);
