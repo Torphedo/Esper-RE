6b87a97533a8dd8da52715a552bc87c35e757a4e
torph <73564623+Torphedo@users.noreply.github.com>
2026-05-07 00:32:08 -0400

    Update DDSX to also convert to DDS

--- src/ddsx/main.c
+++ src/ddsx/main.c
@@ -1,40 +1,129 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
+
 #include <common/file.h>
+#include <common/path.h>
+#include <common/image.h>
+#include <common/dds.h>
+#include <common/arguments.h>
 
-#define DDS_HEADER_SIZE 0x80
+#include <polaris/version.h>
 
-// CLI utility to convert a DDS file to DDX.
-// DDX is used for the font atlas in Phantom Dust, it's just a 1024x2048 BC1
-// texture with no DDS header.
-int main(int argc, char** argv) {
-    if (argc != 3) {
-        // TODO: Add a real error message
-        printf("wrong!\n");
-        return EXIT_FAILURE;
+typedef enum {
+    INVALID, DDS, DDX,
+}filetype;
+
+filetype get_type(const char* path) {
+    if (path_has_extension(path, "ddx")) {
+        return DDX;
     }
-    const char* inpath = argv[1];
-    const char* outpath = argv[2];
+    if (path_has_extension(path, "dds")) {
+        return DDS;
+    }
+    return INVALID;
+}
 
+bool ddx_to_dds(const char* inpath, const char* outpath) {
+    u8* buf = file_load(inpath);
+    if (!buf) {
+        printf("Failed to load input!\n");
+        return false;
+    }
+
+    const texture tex = {
+        .height = 2048,
+        .width = 1024,
+        .compressed = true,
+        .fmt = DXT1,
+        .data = buf,
+    };
+    img_write(tex, outpath);
+
+    free(buf);
+    return true;
+}
+
+bool dds_to_ddx(const char* inpath, const char* outpath) {
     const s64 size = file_size(inpath);
     u8* buf = file_load(inpath);
     if (!buf) {
         printf("Failed to load input!\n");
-        return EXIT_FAILURE;
+        return false;
     }
 
-    const u8* data = buf + DDS_HEADER_SIZE;
-    const s64 outsize = MAX(0, size - DDS_HEADER_SIZE);
+    const u8* data = buf + sizeof(dds_header);
+    const s64 outsize = MAX(0, size - sizeof(dds_header));
 
-    int result = EXIT_FAILURE;
     FILE* f = fopen(outpath, "wb");
     if (f) {
         fwrite(data, outsize, 1, f);
         fclose(f);
-        printf("Wrote trimmed DDX to '%s'\n", outpath);
-        result = EXIT_SUCCESS;
     }
 
     free(buf);
-    return result;
+    return true;
+}
+
+void print_usage(const char* argv0) {
+    printf("usage: %s [input file] [output file]\n", argv0);
+    printf("For example:\n");
+    printf("\t%s Assets/Data/System/dbfont.ddx dbfont.dds\n", argv0);
+    printf("\t%s dbfont.dds Assets/Data/System/dbfont.ddx\n", argv0);
+}
+
+void print_version() {
+    printf("DDSX v1.0 by Torphedo\nOpen-source @ " POLARIS_URL "\n");
+}
+
+// CLI utility to convert a DDS file to DDX and back.
+// DDX is used for the font atlas in Phantom Dust, it's just a 1024x2048 BC1
+// texture with no DDS header.
+int main(int argc, char** argv) {
+    if (argc == 1 || args_getflag(argc, argv, "help", "h")) {
+        print_usage(argv[0]);
+        return EXIT_SUCCESS;
+    }
+
+    if (args_getflag(argc, argv, "version", "v")) {
+        print_version();
+        return EXIT_SUCCESS;
+    }
+
+    if (argc != 3) {
+        print_usage(argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    const char* inpath = argv[1];
+    const char* outpath = argv[2];
+    const filetype intype = get_type(inpath);
+    const filetype outtype = get_type(outpath);
+
+    if (intype == INVALID || outtype == INVALID) {
+        printf("The input or output type is unknown (must be .dds or .ddx)\n");
+        return EXIT_FAILURE;
+    }
+
+    if (intype == outtype) {
+        printf("Both file paths are the same file type, so there's no conversion needed...\n");
+        return EXIT_FAILURE;
+    }
+
+    bool res = false;
+    if (intype == DDS) {
+        assert(outtype == DDX);
+        res = dds_to_ddx(inpath, outpath);
+    } else {
+        assert(intype == DDX && outtype == DDS);
+        res = ddx_to_dds(inpath, outpath);
+    }
+
+    if (res) {
+        printf("Converted '%s' to '%s'\n", inpath, outpath);
+    } else {
+        printf("Failed to convert '%s' to '%s'\n", inpath, outpath);
+    }
+
+    return (res) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
